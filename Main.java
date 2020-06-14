42b568fd267936a752e118b2eeac3873a986a1c2
tolgonayb <66726418+tolgonayb@users.noreply.github.com>
2020-06-14 19:48:35 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+
+        int arr[] = new int[10];
+        for(int i = 0; i <  arr.length; i++) {
+            arr[i] =  (int)(Math.random() * 100);
+            System.out.print(arr[i] + "  ");
+        }
+
+        System.out.print("\nSorted: \n");
+        Arrays.sort(arr);
+        for(int i = 0; i <  arr.length; i++) {
+            System.out.print(arr[i] + "  ");
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
